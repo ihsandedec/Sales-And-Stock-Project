f146cb3575ea20577d51721e0b2282b0268a368d
ddcihsan <ddcihsan@gmail.com>
2023-12-13 19:56:45 +0300

    powers entity created

---
+++ src/tr/com/ih/types/Powers.java
@@ -0,0 +1,30 @@
+package tr.com.ih.types;
+
+public class Powers {
+
+	private int id;
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAdi() {
+		return name;
+	}
+
+	public void setAdi(String adi) {
+		this.name = adi;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return id + " " + name;
+	}
+	
+}
