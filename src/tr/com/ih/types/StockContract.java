0c1301776dc62936d840f22dddf96afccbf42e5f
ddcihsan <ddcihsan@gmail.com>
2023-12-16 15:28:34 +0300

    stock entity created

---
+++ src/tr/com/ih/types/StockContract.java
@@ -0,0 +1,59 @@
+package tr.com.ih.types;
+
+import java.sql.Date;
+
+public class StockContract {
+
+	private int id;
+	private int employeeId;
+	private int productId;
+	private Date date;
+	private int piece;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getPersonelId() {
+		return employeeId;
+	}
+
+	public void setPersonelId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public int getAdet() {
+		return piece;
+	}
+
+	public void setAdet(int piece) {
+		this.piece = piece;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return id + " " + employeeId + " " + productId + " " + piece + " " + date;
+	}
+
+}
