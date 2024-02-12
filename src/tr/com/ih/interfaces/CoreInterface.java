72d5fd65c781eb6f3672a96f53c47cc0c0b8f89b
ihsandedec <ddcihsan@gmail.com>
2024-02-12 22:35:49 +0300

    interface package created and Interface has been defined for the front end.

---
+++ src/tr/com/ih/interfaces/CoreInterface.java
@@ -0,0 +1,5 @@
+package tr.com.ih.interfaces;
+
+public interface CoreInterface {
+
+}
---
+++ src/tr/com/ih/interfaces/FeInterface.java
@@ -0,0 +1,14 @@
+package tr.com.ih.interfaces;
+
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+public interface FeInterface {
+	
+	public void initPencere();
+	public JPanel initPanle();
+	public JMenuBar initBar();
+	public JTabbedPane initTabs();
+
+}
