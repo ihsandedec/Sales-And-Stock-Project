71c3c9e7e06721a5fa28c533017cd07be62b634b
ihsandedec <ddcihsan@gmail.com>
2024-02-13 23:14:34 +0300

    the core layer has been created. connection connections have been made.

---
+++ src/tr/com/ih/core/CoreFields.java
@@ -0,0 +1,25 @@
+package tr.com.ih.core;
+
+public class CoreFields {
+	
+	private String userName = "root";
+	private String password ="";
+	private String url ="private String url =\"jdbc:mysql//localhost/satisvestok?useUnicode=true&characterEncoding=UTF-8\";";
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	
+	
+
+}
