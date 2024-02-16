0ea485580692007094fbfc023c6c2f8cd6e3e645
ihsandedec <ddcihsan@gmail.com>
2024-02-16 22:31:59 +0300

    data acces layer defined

--- src/tr/com/ih/interfaces/DalInterface.java
+++ src/tr/com/ih/interfaces/DalInterface.java
@@ -1,5 +1,19 @@
 package tr.com.ih.interfaces;
 
-public interface DalInterface {
+import java.util.List;
 
+public interface DalInterface<T>
+{
+ //<T> generic interface
+
+ public void Insert (T entitiy); //veritabanindan kayit almak icin
+ 
+ public List<T> GetAll(); //verileri listelemk icin
+ 
+ public T Delete (T entity);  //contract turunde delete
+  
+ public void Update (T entitiy);
+ public List<T> GetById(int id);
+ 
+ 
 }
