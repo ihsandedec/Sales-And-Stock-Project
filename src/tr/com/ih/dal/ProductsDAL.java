ad2d0c8165fc2d9aa39bc4db7ab5fa26cfd51777
ihsandedec <ddcihsan@gmail.com>
2024-02-28 12:10:22 +0300

    ProductsDAL was created

---
+++ src/tr/com/ih/dal/ProductsDAL.java
@@ -0,0 +1,40 @@
+package tr.com.ih.dal;
+
+import java.util.List;
+
+import tr.com.ih.core.ObjectHelper;
+import tr.com.ih.interfaces. DalInterface;
+
+public class ProductsDAL<ProductsContract> extends ObjectHelper implements DalInterface<ProductsContract>{
+
+	@Override
+	public void Insert(ProductsContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<ProductsContract> GetAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProductsContract Delete(ProductsContract entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void Update(ProductsContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<ProductsContract> GetById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
