2714831d3fe0bc9498b631839cba05add36b5f80
ihsandedec <ddcihsan@gmail.com>
2024-02-29 22:43:45 +0300

    StockDAL was created

---
+++ src/tr/com/ih/dal/StockDAL.java
@@ -0,0 +1,40 @@
+package tr.com.ih.dal;
+
+import java.util.List;
+
+import tr.com.ih.core.ObjectHelper;
+import tr.com.ih.interfaces.DalInterface;
+
+public class StockDAL <StockContract> extends ObjectHelper implements DalInterface<StockContract>{
+
+	@Override
+	public void Insert(StockContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<StockContract> GetAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StockContract Delete(StockContract entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void Update(StockContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<StockContract> GetById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
