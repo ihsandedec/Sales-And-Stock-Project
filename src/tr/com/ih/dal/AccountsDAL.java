4f43290ba12cd0579996ed4d5187e91725d4ab2c
ihsandedec <ddcihsan@gmail.com>
2024-02-29 22:41:56 +0300

    AccountsDAL was created

---
+++ src/tr/com/ih/dal/AccountsDAL.java
@@ -0,0 +1,40 @@
+package tr.com.ih.dal;
+
+import java.util.List;
+
+import tr.com.ih.core.ObjectHelper;
+import tr.com.ih.interfaces.DalInterface;
+
+public class AccountsDAL <AccountsContract> extends ObjectHelper implements DalInterface<AccountsContract> {
+
+	@Override
+	public void Insert(AccountsContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<AccountsContract> GetAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AccountsContract Delete(AccountsContract entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void Update(AccountsContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<AccountsContract> GetById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
