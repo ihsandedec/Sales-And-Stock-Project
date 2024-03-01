cbf065d6cc2d9c34ff9433904f143d66d8545330
ihsandedec <ddcihsan@gmail.com>
2024-03-01 22:25:12 +0300

    CustomerDAL was created

---
+++ src/tr/com/ih/dal/CustomerDAL.java
@@ -0,0 +1,40 @@
+package tr.com.ih.dal;
+
+import java.util.List;
+
+import tr.com.ih.core.ObjectHelper;
+import tr.com.ih.interfaces.DalInterface;
+
+public class CustomerDAL <CustomerContract> extends ObjectHelper implements DalInterface<CustomerContract>{
+
+	@Override
+	public void Insert(CustomerContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<CustomerContract> GetAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CustomerContract Delete(CustomerContract entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void Update(CustomerContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<CustomerContract> GetById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
