fd4020a9f0481323a9443902995f870342f65b98
ihsandedec <ddcihsan@gmail.com>
2024-03-05 14:13:12 +0300

    SalesDAL was created

---
+++ src/tr/com/ih/dal/SalesDAL.java
@@ -0,0 +1,40 @@
+package tr.com.ih.dal;
+
+import java.util.List;
+
+import tr.com.ih.core.ObjectHelper;
+import tr.com.ih.interfaces.DalInterface;
+
+public class SalesDAL <SalesContract> extends ObjectHelper implements DalInterface<SalesContract>{
+
+	@Override
+	public void Insert(SalesContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<SalesContract> GetAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SalesContract Delete(SalesContract entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void Update(SalesContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<SalesContract> GetById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
