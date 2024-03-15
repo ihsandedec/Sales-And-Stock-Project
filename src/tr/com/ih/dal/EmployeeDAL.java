b1da83ec90a71280762200cb4b75fb973c88491d
ihsandedec <ddcihsan@gmail.com>
2024-03-15 23:43:44 +0300

    PersonelDAL was converted into EmployeeDAL

--- src/tr/com/ih/dal/EmployeeDAL.java
+++ src/tr/com/ih/dal/EmployeeDAL.java
@@ -5,34 +5,34 @@ import java.util.List;
 import tr.com.ih.core.ObjectHelper;
 import tr.com.ih.interfaces.DalInterface;
 
-public class PersonelDAL <PersonelContract> extends ObjectHelper implements DalInterface<PersonelContract>{
+public class EmployeeDAL <EmployeeContract> extends ObjectHelper implements DalInterface<EmployeeContract>{
 
 	@Override
-	public void Insert(PersonelContract entitiy) {
+	public void Insert(EmployeeContract entitiy) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public List<PersonelContract> GetAll() {
+	public List<EmployeeContract> GetAll() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public PersonelContract Delete(PersonelContract entity) {
+	public EmployeeContract Delete(EmployeeContract entity) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void Update(PersonelContract entitiy) {
+	public void Update(EmployeeContract entitiy) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public List<PersonelContract> GetById(int id) {
+	public List<EmployeeContract> GetById(int id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
