b948bf90d8dcf3c113cf174efd619db31e91f617
ihsandedec <ddcihsan@gmail.com>
2024-03-19 22:13:41 +0300

    A start has been made for the customer interface.

--- src/tr/com/ih/fe/MainWindowFE.java
+++ src/tr/com/ih/fe/MainWindowFE.java
@@ -1,7 +1,9 @@
 package tr.com.ih.fe;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
@@ -21,7 +23,7 @@ public class MainWindowFE extends JFrame implements FeInterface{
 		JMenuBar bar = initBar();
 		
 		//add(tabs);
-		//setJMenuBar(bar);
+		setJMenuBar(bar);
 		
 		setTitle("Sales And Stock Program");
 		pack();
@@ -33,14 +35,35 @@ public class MainWindowFE extends JFrame implements FeInterface{
 
 	@Override
 	public JPanel initPanle() {
-		// TODO Auto-generated method stub
-		return null;
+		JPanel panel = new JPanel();
+		
+		return panel;
 	}
 
 	@Override
 	public JMenuBar initBar() {
-		// TODO Auto-generated method stub
-		return null;
+		JMenuBar bar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		bar.add(fileMenu);
+		
+		JMenuItem exitItem = new JMenuItem("Exit");
+		fileMenu.add(exitItem);
+		
+		
+		
+		JMenu productMenu = new JMenu("Products");
+		bar.add(productMenu);
+		JMenuItem productAddItem = new JMenuItem("Add Product");
+		productMenu.add(productAddItem);
+		JMenuItem categoryAddItem = new JMenuItem("Add Category");
+		productMenu.add(categoryAddItem);
+		productMenu.addSeparator();
+		JMenuItem productEditItem = new JMenuItem("Edit Product");
+		productMenu.add(productEditItem);
+		JMenuItem categoryEditItem = new JMenuItem("Edit Category");
+		productMenu.add(categoryEditItem);
+		
+		return bar;
 	}
 
 	@Override
